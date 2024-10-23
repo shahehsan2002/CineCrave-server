3e4c55f887a6f61c38846f9636dfeb55bfb2efaa
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-23 12:44:53 +0600

    catch async created

--- src/modules/movies/movie.route.ts
+++ src/modules/movies/movie.route.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { MovieControllers } from "./movie.controller";
+import { ReviewControllers } from "../reviews/review.controller";
 
 const router = express.Router();
 
@@ -16,5 +17,16 @@ router.get("/", MovieControllers.getAllMovies);
 
 // get movie by slug route
 router.get("/:slug", MovieControllers.getMovieBySlug);
+
+// add review
+router.post("/:slug/review", ReviewControllers.addReview);
+
+// // get all reviews
+// router.get("/:slug/reviews", ReviewControllers.getAllReviews);
+
+// router.put("/:slug/review", ReviewControllers.getReviewById);
+
+// // delete review
+// router.delete("/:slug/review", ReviewControllers.deleteReview);
 
 export const MovieRoutes = router;
